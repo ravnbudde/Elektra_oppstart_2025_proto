754b4b45fee103c0a64a9de4b0c7db3b89dde38b
ravnbudde <159009459+ravnbudde@users.noreply.github.com>
2024-11-25 15:43:21 +0100

    blink?

--- ATtiny85V/AT/AT/main.c
+++ ATtiny85V/AT/AT/main.c
@@ -6,13 +6,21 @@
  */ 
 
 #include <avr/io.h>
+#include <util/delay.h>
 
 
 int main(void)
 {
+	DDB0 |= (1<<DDB0);
+	
     /* Replace with your application code */
     while (1) 
     {
+		PORTB |= (1<<PB0);
+		_delay_ms(1000);
+		PORTB &= ~(1<<PB0);
+		_delay_ms(1000);
+		
     }
 }
 
