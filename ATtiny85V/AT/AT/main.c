d09588a7b7d94aecc0af6f4815e61304e6a90b01
ravnbudde <159009459+ravnbudde@users.noreply.github.com>
2024-11-25 14:27:49 +0100

    init

---
+++ ATtiny85V/AT/AT/main.c
@@ -0,0 +1,18 @@
+/*
+ * AT.c
+ *
+ * Created: 25/11/2024 14:24:44
+ * Author : Ravn
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
