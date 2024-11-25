d0280e8d2f4d622903be7ddc1678f313f4e9742c
Adriaeik <adriaeik@stud.ntnu.no>
2024-11-25 16:41:26 +0100

    main må til

--- ATtiny85main/main/main/main.c
+++ ATtiny85main/main/main/main.c
@@ -1,8 +1,8 @@
 /*
- * ATtiny85.c
+ * main.c
  *
- * Created: 25/11/2024 14:24:44
- * Author : Ravn
+ * Created: 25.11.2024 16:27:00
+ * Author : eikel
  */ 
 
 #include <avr/io.h>
@@ -13,14 +13,15 @@ int main(void)
 {
 	DDRB |= (1<<DDB0);
 	
-    /* Replace with your application code */
-    while (1) 
-    {
+	/* Replace with your application code */
+	while (1)
+	{
 		PORTB |= (1<<PB0);
 		_delay_ms(1000);
 		PORTB &= ~(1<<PB0);
-		_delay_ms(1000);
+		_delay_ms(500);
 		
-    }
+	}
 }
+
 
