33eacc0e7cd56a236728b5eba9d260ce75a5062b
ravnbudde <159009459+ravnbudde@users.noreply.github.com>
2025-01-11 15:28:52 +0100

    lagd prosjekt for atmega48

---
+++ main/atmega48/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * atmega48.cpp
+ *
+ * Created: 11.01.2025 15:27:31
+ * Author : ravnr
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
