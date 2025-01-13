0add17122cdcaaeb2ceda7a93208fa1dadcdeb11
ravnbudde <159009459+ravnbudde@users.noreply.github.com>
2025-01-13 22:56:14 +0100

    begynt på 48 c kode
    ser nå at det kanskje ikke er c++ som var problemet... har ikke lagd halve init funksjonen engang og flash er nesten 25%

--- ATmega_C/ATmega_C/inc/vl53l0x_i2c_platform.h
+++ ATmega_C/ATmega_C/inc/vl53l0x_i2c_platform.h
@@ -9,7 +9,19 @@
 #ifndef VL53L0X_I2C_PLATFORM_H_
 #include "I2C.h"
 
+struct VL53L0X {
+	uint8_t address;
+	uint8_t stop_variable;
+	uint8_t last_status;
+};
 
+
+
+void VL53L0X_write_mult(struct VL53L0X* dev, uint8_t reg, uint8_t* data, uint8_t count, bool repeat_start);
+void VL53L0X_read_mult(struct VL53L0X* dev, uint8_t reg, uint8_t* buffer, uint8_t count);
+
+void VL53L0X_read_reg(struct VL53L0X* dev, uint8_t reg, uint8_t* buffer);
+void VL53L0X_write_reg(struct VL53L0X* dev, uint8_t reg, uint8_t data);
 
 
 #endif /* VL53L0X_I2C_PLATFORM_H_ */
--- ATmega_C/ATmega_C/main.c
+++ ATmega_C/ATmega_C/main.c
@@ -4,13 +4,18 @@
  * Created: 13.01.2025 13:31:03
  * Author : ravnr
  */ 
-#include "inc/vl53l0x_i2c_platform.h"
+#include "inc/vl53l0x.h"
 
 
 int main(void)
 {
+	struct VL53L0X* device;
+	device->address = 0x29;
+	device->last_status = 0;
+	device->stop_variable = 0;
 
 	tw_init(TW_FREQ_100K, TRUE);
+	VL53L0X_init(device);
 
     while (1) 
     {
