92ad48db201706831a5809444fa61de73cdcc239
ravnbudde <159009459+ravnbudde@users.noreply.github.com>
2025-03-20 16:59:05 +0100

    basic lesing av zumo-sensor og send over UDP til PCen

---
+++ Arduino/test_r4_zumo/src/main.cpp
@@ -0,0 +1,70 @@
+#include <Arduino.h>
+#include <WiFiS3.h>
+#include <Wire.h>
+#include <ZumoShield.h>
+#include <WiFiS3.h>
+#include <WiFiUdp.h>
+
+// I gitignore. Lag en fil under lib, lag SECRET_SSID = nettverk SSID, SECRET_PASS = nettverk passord
+#include "../lib/arduino_secrets.h" 
+
+ZumoIMU imu;
+
+char report[120];
+
+char ssid[] = SECRET_SSID;
+char password[] = SECRET_PASS;
+
+char udpAddr[] = "10.100.35.60";
+int udpPort = 15000;
+
+WiFiUDP udp;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  // Koble til internett
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.print(" \n Tilkoblet internett!");
+  Serial.print("IP-adresse: ");
+  Serial.println(WiFi.localIP());
+
+
+  // Sett opp UDP
+  udp.begin(udpPort);
+
+  // Sett opp imu sensor i bilen
+  Wire.begin();
+  if (!imu.init())
+  {
+    // Failed to detect the compass.
+    while(1)
+    {
+      Serial.println(F("Failed to initialize IMU sensors."));
+      delay(100);
+    }
+  }
+
+  imu.enableDefault();
+}
+
+void loop()
+{
+  // Les sensordata
+  imu.read();
+
+  // Send melding på UDP
+  udp.beginPacket(udpAddr, udpPort);
+  udp.print(imu.a.x);
+  udp.endPacket();
+
+  delay(100);
+}
+
+
---
+++ Programvare/programvare_vips.md
@@ -0,0 +1,10 @@
+# Programmvare som må installeres på Vips-PCene
+
+Greit med en oversikt.
+
+
+- Git: Trengs for å laste ned dependencies i platformio
+
+
+
+
