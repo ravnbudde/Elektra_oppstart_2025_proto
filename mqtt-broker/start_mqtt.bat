a703ebb7fec76ab282aecb922dafd405b14b36f2
Adrian Valaker Eikeland <adriaeik@stud.ntnu.no>
2025-04-01 12:29:55 +0200

    ny epost med pw
    Epost BIAISoppstartsveka@gmail.com
    PW: AutomatiseringOgIntelligenteSystemer

--- mqtt-broker/start_mqtt.bat
+++ mqtt-broker/start_mqtt.bat
@@ -1,5 +1,21 @@
 @echo off
+
+REM === Sjekk om passwd-fila finst ===
+IF NOT EXIST config\passwd (
+    echo Ingen passordfil funne. Startar init_mqtt_passwd.bat...
+    call init_mqtt_passwd.bat
+)
+
+
 call generate_compose.bat
 echo Starting Mosquitto with port from port.txt...
 docker compose up -d
+
+echo.
+echo Venter litt på at containeren skal starte...
+timeout /t 2 >nul
+
+echo Set rettar på mosquitto.db inne i containeren...
+docker exec mosquitto chmod 0700 /mosquitto/data/mosquitto.db
+
 pause
