68243a96579eed0255d7cd310f71790e9540dd58
Adrian Valaker Eikeland <adriaeik@stud.ntnu.no>
2025-04-09 22:25:41 +0200

    debug bash
    debug:
    for /l %i in (1,1,10000) do (
        mosquitto_pub -h 192.168.1.3 -u BIAISbroker -P shinyteapot294 -t "zumo_car/14/sensors/accel/x" -m %i
        timeout /t 1 >nul
    )

--- MATLAB/MQTT_proto/receive_topic/line_Callback.m
+++ MATLAB/MQTT_proto/receive_topic/line_Callback.m
@@ -1,5 +1,5 @@
 function line_Callback(topic, data)
-    sensorData = SensorStorage.getSensor();
+    sensorData = sensor_ref("get");
     sensorData.line = str2double(data);
     fprintf("[line] %s\n", data);
 end
