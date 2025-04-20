ba59bd6852bbcb436dad7c74f6cd87e88eda0a1e
Adriaeik <adriaeik@stud.ntnu.no>
2025-04-21 00:32:18 +0200

    Heiv inn zumoshield biblo lokalt uten buzzer
    ellers fekk eg ikkje kompilere

---
+++ Arduino/test_r4_zumo/lib/ZumoShield/ZumoShield.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <LSM303.h>
+#include <L3G.h>
+#include <Pushbutton.h>
+#include <QTRSensors.h>
+#include <ZumoIMU.h>
+#include <ZumoMotors.h>
+#include <ZumoReflectanceSensorArray.h>
--- Arduino/test_r4_zumo/lib/arduino_secrets.h
+++ Arduino/test_r4_zumo/lib/arduino_secrets.h
@@ -3,15 +3,15 @@
 #define CAR_ID "14"
 
 // Network SSID 
-#define SECRET_SSID "BIAIS_OppstartsVeka" 
+#define SECRET_SSID_WIFI "BIAIS_OppstartsVeka" 
 // Network password
-#define SECRET_PASS "shinyteapot294"
+#define SECRET_PASS_WIFI "shinyteapot294"
 
 
 // IP of server with mqtt broker
 #define MQTT_SERVER "192.168.1.3"
 // Port to MQTT at server
-#define MQTT_PORT 1883;
+#define MQTT_PORT 1883
 // Username for MQTT
 #define MQTT_USER "BIAISbroker" 
 // Password for MQTT
