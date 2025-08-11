684d672fb18957df9ca38d8b2c16958aba9aecca
Adrian Valaker Eikeland <adriaeik@stud.ntnu.no>
2025-08-11 17:36:16 +0200

    r

--- MATLAB/lib_oppstartsveka/mqtt/mqtt_simulink_lib/24a/id_setup.m
+++ MATLAB/lib_oppstartsveka/mqtt/mqtt_simulink_lib/24a/id_setup.m
@@ -8,6 +8,10 @@ classdef id_setup
         
             car_id = (maskWS.get('carID'));
             group_id = (maskWS.get('groupID'));
+            % Assert that id is not zero
+            % assert(car_id ~= 0, 'Error: car_id cannot be zero');
+            % assert(group_id ~= 0, 'Error: group_id cannot be zero');
+            
             assignin('base', 'car_id', car_id);
             assignin('base', 'group_id', group_id);
         end
